c0bcd39d04f6413ba073df6a4f55e29506f4fcf4
FVB-Oaf <168017987+FVB-Oaf@users.noreply.github.com>
2025-05-10 08:53:27 -0400

    Update jump.js

--- webassets/jump.js
+++ webassets/jump.js
@@ -1,2 +1,4 @@
-const version="1.1.0";
-window.onload=function(){document.getElementById("version").innerHTML=`${version}`;};
+const version = "May 10<sup>th</sup>, 2025";
+window.onload = function(){
+  document.getElementById("version").innerHTML = `${version}`;
+};
